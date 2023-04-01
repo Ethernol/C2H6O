e812ad5f73a5ab6c2ee453b948b9d7e09239407f
Louis Dorweiler <louis.dorweiler@gmx.de>
2023-04-01 18:46:27 +0200

    Add working style changes.

--- ethernol/src/routes/create/+page.svelte
+++ ethernol/src/routes/create/+page.svelte
@@ -1,103 +1,103 @@
-<script>
-	import { onMount } from "svelte";
-	import { copy } from 'svelte-copy';
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { copy } from 'svelte-copy';
 
-	import Web3 from 'web3';
-	
+    // import type Web3 from 'web3';
+    // let smartContractAddress = '0x472eCED37080fbCcb2332562f69B13e6d1c658cA';
+    let metaMaskButtonString = '';
+    let account = '';
+    let shortname = '';
 
-	let smartContractAddress = "0x472eCED37080fbCcb2332562f69B13e6d1c658cA";
+    let connected = false;
 
-	let metaMaskButtonString = "";
-	let connected = false;
+    // let web3: Web3;
+    // let smartContractInstance;
+    let base_link = window.location.href;
+    base_link = base_link.replace('create', 'contribute');
+    base_link += '?target=';
 
-	let account = "";
-	let smartContractInstance;
-	let web3;
-	let shortname  = "";
-	let base_link = window.location.href;
-	base_link = base_link.replace("create", "contribute"); 
-	base_link += "?target=";
-	let link = "";
-	let link_created = false;
-	let width = 10;
-	let heigth = 10;
-	let ppp = 100000000;
+    let link = '';
+    let link_created = false;
 
+    let width = 10;
+    let heigth = 10;
+    let ppp = 100000000;
 
-	onMount(async () => {
-		metaMaskButtonString = checkMetaMask();
+    onMount(async () => {
+        metaMaskButtonString = checkMetaMask();
 
-		function checkMetaMask() {
-			if (typeof window.ethereum == 'undefined') {			
-				console.log('MetaMask is not installed!');
-				return 'MetaMask is not installed.\nClick here to install!';
-			}
-			return 'Click to login with MetaMask';
-		}
-		
+        function checkMetaMask() {
+            if (typeof window.ethereum == 'undefined') {
+                console.log('MetaMask is not installed!');
+                return 'MetaMask is not installed.\nClick here to install!';
+            }
+            return 'Click to login with MetaMask';
+        }
     });
 
-	async function onMetaMaskButton(){
-		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-		account = accounts[0];
-		connected = true;
-		setSmartContractInstance();
-	}
-	
-	function createLink(){
-		link_created = true;
-		if (shortname === ""){
-			link = base_link + account;
-		}
-		else{
-			link = base_link + shortname;
-		}
-	}
+    async function onMetaMaskButton() {
+        const accounts = await window.ethereum.request({
+            method: 'eth_requestAccounts'
+        });
+        account = accounts[0];
+        connected = true;
+        // TODO: Does not work
+        // setSmartContractInstance();
+    }
 
+    function createLink() {
+        link_created = true;
+        if (shortname === '') {
+            link = base_link + account;
+        } else {
+            link = base_link + shortname;
+        }
+    }
 </script>
+
 <svelte:head>
-	<title>Create</title>
-	<meta name="description" content="Create" />
+    <title>Create</title>
+    <meta name="description" content="Create" />
 </svelte:head>
 
 <section>
-	<h1> Create your Image </h1>
-		<span class="welcome">
-			<div class="center">
-				{#if !connected}
-				<div class="center">
-					<button class="button-22" on:click={onMetaMaskButton}>
-						{metaMaskButtonString}
-					</button>
-				</div>
-				{/if}
-				{#if connected}
-					<h3> Your Wallet:</h3>
-					<input type="text" bind:value={account} placeholder={account} readonly>
-					<h3> Enter Shortname:</h3>
-					<input type="text" bind:value={shortname}>
-					<h3> Image width:</h3>
-					<input type="text" bind:value={width}>
-					<h3> Image heigth:</h3>
-					<input type="text" bind:value={heigth}>
-					<h3> Price per pixel:</h3>
-					<input type="text" bind:value={ppp}>
-					
-					{#if !link_created}
-						<button class="button-22" on:click={createLink}>
-							Create Link
-						</button>
-					{/if}
-					{#if link_created}
-						<h3> Link:</h3>
-						<h2>{link}</h2>
-						<button class="button-22" use:copy={link}>
-							Copy!
-						</button>
-					{/if}
-				{/if}
-			</div>
-		</span>
-</section>
-<div id="clipboard"></div>
+    <h1>Create your Image</h1>
+    <span class="welcome">
+        <div class="center">
+            {#if !connected}
+                <div class="center">
+                    <button class="button-22" on:click={onMetaMaskButton}>
+                        {metaMaskButtonString}
+                    </button>
+                </div>
+            {:else}
+                <h3>Your Wallet:</h3>
+                <input
+                    type="text"
+                    bind:value={account}
+                    placeholder={account}
+                    readonly
+                />
+                <h3>Enter Shortname:</h3>
+                <input type="text" bind:value={shortname} />
+                <h3>Image width:</h3>
+                <input type="text" bind:value={width} />
+                <h3>Image heigth:</h3>
+                <input type="text" bind:value={heigth} />
+                <h3>Price per pixel:</h3>
+                <input type="text" bind:value={ppp} />
 
+                {#if !link_created}
+                    <button class="button-22" on:click={createLink}>
+                        Create Link
+                    </button>
+                {:else}
+                    <h3>Link:</h3>
+                    <h2>{link}</h2>
+                    <button class="button-22" use:copy={link}> Copy! </button>
+                {/if}
+            {/if}
+        </div>
+    </span>
+</section>
+<div id="clipboard" />
