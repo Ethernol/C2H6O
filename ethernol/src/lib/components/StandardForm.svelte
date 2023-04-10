f37a8b5fb59cad9f3b7bac89236d54a6de64eb0e
Louis Dorweiler <louis.dorweiler@gmx.de>
2023-04-10 15:09:12 +0200

    Add form component.

---
+++ ethernol/src/lib/components/StandardForm.svelte
@@ -0,0 +1,8 @@
+<div class="w-full max-w-md m-3">
+    <form
+        class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        autocomplete="off"
+    >
+        <slot />
+    </form>
+</div>
--- ethernol/src/routes/contribute/+page.svelte
+++ ethernol/src/routes/contribute/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
     import ConnectionStatusLine from '$lib/components/ConnectionStatusLine.svelte';
+    import StandardInput from '$lib/components/StandardInput.svelte';
     import StandardButton from '$lib/components/StandardButton.svelte';
+    import StandardForm from '$lib/components/StandardForm.svelte';
+
+    import { creationStateController } from '$lib/script/creation_state_controller';
+    const { pricePerPixel } = creationStateController;
 
     import { appStateController } from '$lib/script/app_state_controller';
     const { userAccount } = appStateController;
@@ -15,42 +20,60 @@
         guide,
         colorInput,
         toggleGuide,
-        remover
+        remover,
+        changedPixels
     } = canvasStateController;
 </script>
 
 <ConnectionStatusLine title="Send">
-    <div id="guide" bind:this={$guide} />
+    <StandardForm>
+        <StandardInput
+            type="color"
+            bind:this={$colorInput}
+            placeholder={$colorInput}
+            id="colorInput"
+            label="Set Color:"
+        />
+        <StandardInput
+            type="checkbox"
+            bind:this={$toggleGuide}
+            placeholder={$toggleGuide}
+            id="toggleGuide"
+            label="Show Guide:"
+            checked
+        />
+        <StandardInput
+            type="checkbox"
+            bind:this={$remover}
+            placeholder={$remover}
+            id="toggleRemover"
+            label="Remove Pixel:"
+            checked
+        />
+        {#if $userAccount !== '' && !$loaded}
+            <div class="center">
+                <StandardButton
+                    type="text"
+                    bind:value={$imageAddress}
+                    placeholder={$imageAddress}
+                />
+                <StandardButton
+                    toggleHandle="getFanImage"
+                    label="Open Fan Image."
+                />
+            </div>
+        {/if}
 
-    <canvas width="0" height="0" bind:this={$canvas} id="canvas" />
-
-    <label for="colorInput">Set Color: </label>
-    <input type="color" bind:this={$colorInput} id="colorInput" />
-    <label for="toggleGuide">Show Guide: </label>
-    <input type="checkbox" bind:this={$toggleGuide} checked id="toggleGuide" />
-    <label for="toggleRemover">Remove Pixel: </label>
-    <input type="checkbox" bind:this={$remover} id="toggleRemover" />
-
-    {#if $userAccount !== '' && !$loaded}
-        <div class="center">
-            <input
-                type="text"
-                bind:value={$imageAddress}
-                placeholder={$imageAddress}
-            />
+        {#if $loaded && $ready}
             <StandardButton
-                toggleHandle="getFanImage"
-                label="Open Fan Image."
+                toggleHandle="paintPixels"
+                label="Paint pixels (for {Object.keys($changedPixels).length *
+                    $pricePerPixel} + WEI.)."
             />
-        </div>
-    {/if}
-
-    {#if $loaded && $ready}
-        <div class="center">
-            <StandardButton toggleHandle="paintPixels" />
-            <!-- label="Paint pixels (for {Object.keys($changedPixels).length *$pricePerPixel} + WEI.)."/> -->
-        </div>
-    {/if}
+        {/if}
+    </StandardForm>
+    <div id="guide" bind:this={$guide} />
+    <canvas width="0" height="0" bind:this={$canvas} id="canvas" />
 </ConnectionStatusLine>
 
 <style>
