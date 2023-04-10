abd2fd5750b281e82b904a7444561e2ed7745c43
Louis Dorweiler <louis.dorweiler@gmx.de>
2023-04-10 18:55:39 +0200

    Switch back to working state with old css for contribute page.

--- ethernol/src/lib/components/StandardInput.svelte
+++ ethernol/src/lib/components/StandardInput.svelte
@@ -1,26 +1,51 @@
 <script lang="ts">
     export let type = 'text';
-    export let value: string | number = '';
+    export let value: string | number | boolean = '';
     export let placeholder = value as string;
     export let id = '';
     export let readonly = false;
     export let label = '';
     export let hidden = false;
+    export let checked = false;
 </script>
 
-<label
-    class="text-left block text-gray-700 text-sm font-bold mt-2 mb-2"
-    for={id}>{label}</label
->
-<input
-    {type}
-    {placeholder}
-    {value}
-    {id}
-    class="shadow appearance-none border 
+{#if type === 'checkbox'}
+    <div class="flex items-center">
+        <label
+            class="text-left block text-gray-700 text-sm font-bold my-2 mr-5"
+            for={id}
+        >
+            {label}
+        </label>
+        <input class="form-checkbox h-5 w-5" {type} {value} {id} {checked} />
+    </div>
+{:else if type === 'color'}
+    <div class="flex items-center">
+        <label
+            class="text-left block text-gray-700 text-sm font-bold my-2 mr-5"
+            for={id}>{label}</label
+        >
+        <input
+            class="bg-white border border-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+            {type}
+            {id}
+        />
+    </div>
+{:else}
+    <label
+        class="text-left block text-gray-700 text-sm font-bold mt-2 mb-2"
+        for={id}>{label}</label
+    >
+    <input
+        {type}
+        {placeholder}
+        {value}
+        {id}
+        class="shadow appearance-none border 
                 rounded w-full py-2 px-3 text-gray-700
                 leading-tight
                 focus:outline-none focus:shadow-outline"
-    {readonly}
-    {hidden}
-/>
+        {readonly}
+        {hidden}
+    />
+{/if}
