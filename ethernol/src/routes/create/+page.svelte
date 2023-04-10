125673e26cd71aeba6915cf81ed55b21067e5685
Louis Dorweiler <louis.dorweiler@gmx.de>
2023-04-10 15:09:12 +0200

    Add form component.

--- ethernol/src/routes/create/+page.svelte
+++ ethernol/src/routes/create/+page.svelte
@@ -2,6 +2,7 @@
     import ConnectionStatusLine from '$lib/components/ConnectionStatusLine.svelte';
     import StandardInput from '$lib/components/StandardInput.svelte';
     import StandardButton from '$lib/components/StandardButton.svelte';
+    import StandardForm from '$lib/components/StandardForm.svelte';
 
     import { appStateController } from '$lib/script/app_state_controller';
     const { userAccount } = appStateController;
@@ -12,56 +13,51 @@
 </script>
 
 <ConnectionStatusLine title="Create Your Image">
-    <div class="w-full max-w-md m-3">
-        <form
-            class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            autocomplete="off"
-        >
+    <StandardForm>
+        <StandardInput
+            bind:value={$userAccount}
+            placeholder={$userAccount}
+            id="userAccount"
+            label="User Account:"
+            readonly
+        />
+
+        <StandardInput
+            placeholder={$shortName}
+            id="userAccount"
+            label="Enter Shortname:"
+        />
+
+        <StandardInput
+            type="number"
+            value={$width}
+            id="width"
+            label="Image width:"
+        />
+
+        <StandardInput
+            type="number"
+            value={$height}
+            id="height"
+            label="Image height:"
+        />
+
+        <StandardInput
+            type="number"
+            value={$pricePerPixel}
+            id="ppp"
+            label="Price per Pixel:"
+        />
+        {#if $link === ''}
+            <StandardButton toggleHandle="createLink" label="Create Link" />
+        {:else}
             <StandardInput
-                bind:value={$userAccount}
-                placeholder={$userAccount}
-                id="userAccount"
-                label="User Account:"
+                id={$link}
+                placeholder={$link}
+                label="Link:"
                 readonly
             />
-
-            <StandardInput
-                placeholder={$shortName}
-                id="userAccount"
-                label="Enter Shortname:"
-            />
-
-            <StandardInput
-                type="number"
-                value={$width}
-                id="width"
-                label="Image width:"
-            />
-
-            <StandardInput
-                type="number"
-                value={$height}
-                id="height"
-                label="Image height:"
-            />
-
-            <StandardInput
-                type="number"
-                value={$pricePerPixel}
-                id="ppp"
-                label="Price per Pixel:"
-            />
-            {#if $link === ''}
-                <StandardButton toggleHandle="createLink" label="Create Link" />
-            {:else}
-                <StandardInput
-                    id={$link}
-                    placeholder={$link}
-                    label="Link:"
-                    readonly
-                />
-                <StandardButton label="Copy!" content={$link} />
-            {/if}
-        </form>
-    </div>
+            <StandardButton label="Copy!" content={$link} />
+        {/if}
+    </StandardForm>
 </ConnectionStatusLine>
